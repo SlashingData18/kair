c043402a7913fcc19537c3369da5852f86248f4d
Kyle Menta <71467967+SlashingData18@users.noreply.github.com>
2025-11-29 19:05:05 -0500

    Clean up imports in __init__.py
    Removed unused imports from __init__.py

--- kair/__init__.py
+++ kair/__init__.py
@@ -1,6 +1,4 @@
 from . import models as models
 from . import utils as utils
 from . import scripts as scripts
-from . import data_preparation as data_preparation
 from . import retinaface as retinaface
-#from . import main as main
